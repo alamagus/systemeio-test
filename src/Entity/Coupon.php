5411e82678e64ea28bd3624a0fdb9a24864869b4
Samir Mameev <alamagus@yandex.ru>
2025-09-29 09:58:24 +0300

    create entities and migration

---
+++ src/Entity/Coupon.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\CouponType;
+use App\Repository\CouponRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CouponRepository::class)]
+class Coupon
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $code = null;
+
+    #[ORM\Column(enumType: CouponType::class)]
+    private ?CouponType $type = null;
+
+    #[ORM\Column]
+    private ?int $value = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getType(): ?CouponType
+    {
+        return $this->type;
+    }
+
+    public function setType(CouponType $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getValue(): ?int
+    {
+        return $this->value;
+    }
+
+    public function setValue(int $value): static
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
